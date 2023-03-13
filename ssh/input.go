f0dd794376f097604c32f1a2e45261bc175c72a2
prasadachar08 <prasadachar05@gmail.com>
2023-03-13 11:54:45 +0530

    first commit

---
+++ ssh/input.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main(){
+fmt.Println("Hi hwllow world")
+fmt.Println("Enter the number")
+var val int
+fmt.Scanln(&val)
+fmt.Println("Entered number is : ",val)
+}
